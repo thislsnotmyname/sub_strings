a8289e9eb3b074c1504937f121f1a8db3f1aa16e
JM <143664295+thislsnotmyname@users.noreply.github.com>
2024-07-08 11:59:27 -0500

    Add logic to method and user input

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # sub_strings
+This project is to create a Ruby method that counts how many times a given list of substrings occurs in a user-inputted string.
+This project was made as an exercise for [The Odin Project](https://www.theodinproject.com/lessons/ruby-sub-strings).
+> JM
--- substrings.rb
+++ substrings.rb
@@ -1,6 +1,18 @@
 DICTIONARY = %w[below down go going horn how howdy it i low own part partner sit]
 def substrings(string, dict)
-
+  string_split = string.downcase.split("")
+  sub_amount = Hash.new(0)
+  dict.each do |word|
+    range_end = word.length - 1
+    string_split.each_index do |idx|
+      sub_amount[word] += 1 if string_split[idx..range_end].reduce(:+) == word
+      range_end += 1
+      break if string_split[idx..range_end].reduce(:+).length < word.length
+    end
+  end
+  sub_amount
 end
-p DICTIONARY
-#substrings("Hello", DICTIONARY)
+print "Please input a string to be tested against the dictionary: "
+string = gets.chomp.to_s
+puts "The string: \"" << string << "\" contains these substrings:"
+puts substrings(string, DICTIONARY)
